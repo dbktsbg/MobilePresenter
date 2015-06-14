6f42282fd9b7a7faa6982c8fc06f6a2d2ee7929e
dbktsbg <dbkdev50@gmail.com>
2015-06-14 12:45:56 -0400

    v1.2
    Tool bar

--- www/js/Index.js
+++ www/js/Index.js
@@ -1,31 +1,131 @@
 
-var app = 
+jQuery
+(
+    function ()
     {
-        initialize: function ()
+
+        var app =
         {
-            this.bindEvents();
-        },
+            initialize: function () {
+                this.bindEvents();
+            },
 
-        bindEvents: function ()
-        {
-            document.addEventListener('deviceready', this.onDeviceReady, false);
-        },
+            bindEvents: function () {
+                document.addEventListener('deviceready', this.onDeviceReady, false);
+            },
 
-        onDeviceReady: function ()
-        {
-            app.receivedEvent('deviceready');
-        },
+            onDeviceReady: function () {
+                app.receivedEvent('deviceready');
+            },
 
-        receivedEvent: function (id)
-        {
-            var parentElement = document.getElementById(id);
-            var listeningElement = parentElement.querySelector('.listening');
-            var receivedElement = parentElement.querySelector('.received');
+            receivedEvent: function (id) {
+                var parentElement = document.getElementById(id);
+                var listeningElement = parentElement.querySelector('.listening');
+                var receivedElement = parentElement.querySelector('.received');
 
-            listeningElement.setAttribute('style', 'display:none;');
-            receivedElement.setAttribute('style', 'display:block;');
+                listeningElement.setAttribute('style', 'display:none;');
+                receivedElement.setAttribute('style', 'display:block;');
 
-            console.log('Received Event: ' + id);
+                console.log('Received Event: ' + id);
+            }
+
         }
 
+        MobilePresenterViewModel = 
+            function ()
+            {
+
+                // ========== Enumerations ==========
+
+                // ========== Properties ==========
+                var self = this;
+
+                // ========== Event Handlers =========
+
+                // ========== Methods =========
+
+                // ---------- App ----------
+
+                self.initializeViewModel =
+                    function () {
+                        try {
+                            initializeAppState();
+                            initializeUI();
+                        }
+                        catch (e) {
+                            error(e.message);
+                        }
+                    }
+
+                initializeAppState =
+                    function () {
+                        try {
+                        }
+                        catch (e) {
+                            error(e.message);
+                        }
+                    }
+
+                initializeUI =
+                    function () {
+                        try {
+                            //jQuery("#SlideViewerParentContainter")
+                            //    .swipe
+                            //    (
+                            //        {
+                            //            swipe: swipeHandler(event, direction, distance, duration, fingerCount, fingerData),
+                            //            threshold: 0
+                            //        }
+                            //    );
+                        }
+                        catch (e) {
+                            error(e.message);
+                        }
+                    }
+
+                error =
+                    function (message)
+                    {
+                        alert(message);
+                    }
+
+                // ---------- Slide Navigation ----------
+
+                //swipeHandler = 
+                //    function (event, direction, distance, duration, fingerCount, fingerData) 
+                //    {
+                //        alert("swipeHandler()");
+                //        switch (direction)
+                //        {
+                //            case "left":
+                //                OnNextSlide();
+                //                break;
+                //            case "right":
+                //                OnPreviousSlide();
+                //                break;
+                //            default:
+                //                alert("unhandled swipe direction:" + direction);
+                //                break;
+                //        }
+                //    }
+
+                OnNextSlide =
+                        function () {
+                            alert("OnNextSlide");
+                        }
+
+                OnPreviousSlide =
+                    function () {
+                        alert("OnNextSlide");
+                    }
+
+            }
+
+        // ========== Main =========
+        var myMobilePresenterViewModel = new MobilePresenterViewModel();
+        myMobilePresenterViewModel.initializeViewModel();
+
+        app.initialize();
+
     }
+);
